6cd4a3df5af4e5b6914a8204ecb368470779b811
Andreas Hohler (ah12z468) <andreas.hohler@students.unibe.ch>
2016-09-28 12:02:01 +0200

    init maven & spring w/ twitter bootstrap

---
+++ src/main/java/ch/unibe/scg/BigCommitAnalysisApplication.java
@@ -0,0 +1,12 @@
+package ch.unibe.scg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigCommitAnalysisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BigCommitAnalysisApplication.class, args);
+	}
+}
