618ef427315768246b368c164dcfe506f40c2e32
Andreas Hohler (ah12z468) <andreas.hohler@students.unibe.ch>
2017-02-03 16:01:39 +0100

    updated db structure

--- db_structure.sql
+++ db_structure.sql
@@ -18,175 +18,6 @@ USE `bico`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `batch_job_execution`
---
-
-DROP TABLE IF EXISTS `batch_job_execution`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `batch_job_execution` (
-  `JOB_EXECUTION_ID` bigint(20) NOT NULL,
-  `VERSION` bigint(20) DEFAULT NULL,
-  `JOB_INSTANCE_ID` bigint(20) NOT NULL,
-  `CREATE_TIME` datetime NOT NULL,
-  `START_TIME` datetime DEFAULT NULL,
-  `END_TIME` datetime DEFAULT NULL,
-  `STATUS` varchar(10) DEFAULT NULL,
-  `EXIT_CODE` varchar(2500) DEFAULT NULL,
-  `EXIT_MESSAGE` varchar(2500) DEFAULT NULL,
-  `LAST_UPDATED` datetime DEFAULT NULL,
-  `JOB_CONFIGURATION_LOCATION` varchar(2500) DEFAULT NULL,
-  PRIMARY KEY (`JOB_EXECUTION_ID`),
-  KEY `JOB_INST_EXEC_FK` (`JOB_INSTANCE_ID`),
-  CONSTRAINT `JOB_INST_EXEC_FK` FOREIGN KEY (`JOB_INSTANCE_ID`) REFERENCES `batch_job_instance` (`JOB_INSTANCE_ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `batch_job_execution_context`
---
-
-DROP TABLE IF EXISTS `batch_job_execution_context`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `batch_job_execution_context` (
-  `JOB_EXECUTION_ID` bigint(20) NOT NULL,
-  `SHORT_CONTEXT` varchar(2500) NOT NULL,
-  `SERIALIZED_CONTEXT` text,
-  PRIMARY KEY (`JOB_EXECUTION_ID`),
-  CONSTRAINT `JOB_EXEC_CTX_FK` FOREIGN KEY (`JOB_EXECUTION_ID`) REFERENCES `batch_job_execution` (`JOB_EXECUTION_ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `batch_job_execution_params`
---
-
-DROP TABLE IF EXISTS `batch_job_execution_params`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `batch_job_execution_params` (
-  `JOB_EXECUTION_ID` bigint(20) NOT NULL,
-  `TYPE_CD` varchar(6) NOT NULL,
-  `KEY_NAME` varchar(100) NOT NULL,
-  `STRING_VAL` varchar(250) DEFAULT NULL,
-  `DATE_VAL` datetime DEFAULT NULL,
-  `LONG_VAL` bigint(20) DEFAULT NULL,
-  `DOUBLE_VAL` double DEFAULT NULL,
-  `IDENTIFYING` char(1) NOT NULL,
-  KEY `JOB_EXEC_PARAMS_FK` (`JOB_EXECUTION_ID`),
-  CONSTRAINT `JOB_EXEC_PARAMS_FK` FOREIGN KEY (`JOB_EXECUTION_ID`) REFERENCES `batch_job_execution` (`JOB_EXECUTION_ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `batch_job_execution_seq`
---
-
-DROP TABLE IF EXISTS `batch_job_execution_seq`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `batch_job_execution_seq` (
-  `ID` bigint(20) NOT NULL,
-  `UNIQUE_KEY` char(1) NOT NULL,
-  UNIQUE KEY `UNIQUE_KEY_UN` (`UNIQUE_KEY`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `batch_job_instance`
---
-
-DROP TABLE IF EXISTS `batch_job_instance`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `batch_job_instance` (
-  `JOB_INSTANCE_ID` bigint(20) NOT NULL,
-  `VERSION` bigint(20) DEFAULT NULL,
-  `JOB_NAME` varchar(100) NOT NULL,
-  `JOB_KEY` varchar(32) NOT NULL,
-  PRIMARY KEY (`JOB_INSTANCE_ID`),
-  UNIQUE KEY `JOB_INST_UN` (`JOB_NAME`,`JOB_KEY`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `batch_job_seq`
---
-
-DROP TABLE IF EXISTS `batch_job_seq`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `batch_job_seq` (
-  `ID` bigint(20) NOT NULL,
-  `UNIQUE_KEY` char(1) NOT NULL,
-  UNIQUE KEY `UNIQUE_KEY_UN` (`UNIQUE_KEY`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `batch_step_execution`
---
-
-DROP TABLE IF EXISTS `batch_step_execution`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `batch_step_execution` (
-  `STEP_EXECUTION_ID` bigint(20) NOT NULL,
-  `VERSION` bigint(20) NOT NULL,
-  `STEP_NAME` varchar(100) NOT NULL,
-  `JOB_EXECUTION_ID` bigint(20) NOT NULL,
-  `START_TIME` datetime NOT NULL,
-  `END_TIME` datetime DEFAULT NULL,
-  `STATUS` varchar(10) DEFAULT NULL,
-  `COMMIT_COUNT` bigint(20) DEFAULT NULL,
-  `READ_COUNT` bigint(20) DEFAULT NULL,
-  `FILTER_COUNT` bigint(20) DEFAULT NULL,
-  `WRITE_COUNT` bigint(20) DEFAULT NULL,
-  `READ_SKIP_COUNT` bigint(20) DEFAULT NULL,
-  `WRITE_SKIP_COUNT` bigint(20) DEFAULT NULL,
-  `PROCESS_SKIP_COUNT` bigint(20) DEFAULT NULL,
-  `ROLLBACK_COUNT` bigint(20) DEFAULT NULL,
-  `EXIT_CODE` varchar(2500) DEFAULT NULL,
-  `EXIT_MESSAGE` varchar(2500) DEFAULT NULL,
-  `LAST_UPDATED` datetime DEFAULT NULL,
-  PRIMARY KEY (`STEP_EXECUTION_ID`),
-  KEY `JOB_EXEC_STEP_FK` (`JOB_EXECUTION_ID`),
-  CONSTRAINT `JOB_EXEC_STEP_FK` FOREIGN KEY (`JOB_EXECUTION_ID`) REFERENCES `batch_job_execution` (`JOB_EXECUTION_ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `batch_step_execution_context`
---
-
-DROP TABLE IF EXISTS `batch_step_execution_context`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `batch_step_execution_context` (
-  `STEP_EXECUTION_ID` bigint(20) NOT NULL,
-  `SHORT_CONTEXT` varchar(2500) NOT NULL,
-  `SERIALIZED_CONTEXT` text,
-  PRIMARY KEY (`STEP_EXECUTION_ID`),
-  CONSTRAINT `STEP_EXEC_CTX_FK` FOREIGN KEY (`STEP_EXECUTION_ID`) REFERENCES `batch_step_execution` (`STEP_EXECUTION_ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `batch_step_execution_seq`
---
-
-DROP TABLE IF EXISTS `batch_step_execution_seq`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `batch_step_execution_seq` (
-  `ID` bigint(20) NOT NULL,
-  `UNIQUE_KEY` char(1) NOT NULL,
-  UNIQUE KEY `UNIQUE_KEY_UN` (`UNIQUE_KEY`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
 -- Table structure for table `commitfiles`
 --
 
@@ -205,7 +36,7 @@ CREATE TABLE `commitfiles` (
   PRIMARY KEY (`id`),
   KEY `FK_6dpl7twcby705uampcsyfyde` (`commit_id`),
   CONSTRAINT `FK_6dpl7twcby705uampcsyfyde` FOREIGN KEY (`commit_id`) REFERENCES `commits` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2046391 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -224,7 +55,7 @@ CREATE TABLE `commitissues` (
   PRIMARY KEY (`id`),
   KEY `FK_6v0jpksndolnqc7rsafob1l8k` (`commit_id`),
   CONSTRAINT `FK_6v0jpksndolnqc7rsafob1l8k` FOREIGN KEY (`commit_id`) REFERENCES `commits` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=121935 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -247,7 +78,7 @@ CREATE TABLE `commits` (
   KEY `FK_9trykgj1m8q5mqmh5x4u2nxq9` (`project_id`),
   CONSTRAINT `FK_9trykgj1m8q5mqmh5x4u2nxq9` FOREIGN KEY (`project_id`) REFERENCES `projects` (`id`),
   CONSTRAINT `FK_lvba9otgogq9bg4h2xeq98myp` FOREIGN KEY (`parentCommit_id`) REFERENCES `commits` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=202069 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -265,7 +96,7 @@ CREATE TABLE `projects` (
   `type` int(11) DEFAULT NULL,
   `url` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
