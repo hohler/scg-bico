7a417fb239979e3527067b37bcc6eaa5511753a8
Haidar Osman <osman@inf.unibe.ch>
2017-06-25 12:33:14 +0200

    - added 2 minutes sleep - added two sql scripts for easy delete and drop database content

--- src/main/java/tool/bico/repository/GitHubAPI.java
+++ src/main/java/tool/bico/repository/GitHubAPI.java
@@ -23,20 +23,20 @@ public class GitHubAPI {
 	private String name;
 	private String owner;
 	private GitHubClient client;
-	
+
 	private Map<String, CommitIssue.Type> types;
 	private Map<String, CommitIssue.Priority> priorities;
-	
+
 	public GitHubAPI(String gitHubUrl) {
 		client = new GitHubClient();
 		client.setOAuth2Token("df5f225320a07d1de361bb3493d1f8aedb43fbfc");
-		
+
 		RepositoryService service = new RepositoryService(client);
-		
+
 		String[] splitted = gitHubUrl.split("/");
-		name = splitted[splitted.length-1];
-		owner = splitted[splitted.length-2];
-		
+		name = splitted[splitted.length - 1];
+		owner = splitted[splitted.length - 2];
+
 		try {
 			repository = service.getRepository(owner, name);
 		} catch (IOException e) {
@@ -46,158 +46,153 @@ public class GitHubAPI {
 		types = new HashMap<>();
 		loadLabels();
 	}
-	
+
 	private void loadLabels() {
 		LabelService service = new LabelService(client);
 		try {
 			List<Label> labels = service.getLabels(repository);
-			for(Label l : labels) {
+			for (Label l : labels) {
 				String name = l.getName().toLowerCase();
-				if(name.startsWith(":")) continue;
+				if (name.startsWith(":"))
+					continue;
 				SwitchSubstring.of(name)
-					//Priorities
-					.when("blocker").then(() -> priorities.put(name, CommitIssue.Priority.BLOCKER))
-					.when("breaking", "breaking-java", "pretty blooding important", "critical").then(() -> priorities.put(name,  CommitIssue.Priority.CRITICAL))
-					.when("major", "severity").then(() -> priorities.put(name, CommitIssue.Priority.MAJOR))
-					.when("minor").then(() -> priorities.put(name, CommitIssue.Priority.MINOR))
-					.when("trivial").then(() -> priorities.put(name, CommitIssue.Priority.TRIVIAL))
-					//Types
-					.when("bug").then(() -> types.put(name, CommitIssue.Type.BUG))
-					.when("deprecation").then(() -> types.put(name, CommitIssue.Type.DEPRECATION))
-					.when("refactor", "change").then(() -> types.put(name, CommitIssue.Type.REFACTOR))
-					.when("docs", "documentation", "doc").then(() -> types.put(name, CommitIssue.Type.DOCUMENTATION))
-					.when("feature").then(() -> types.put(name, CommitIssue.Type.FEATURE))
-					.when("enhancement", "optimization", "improvement").then(() -> types.put(name, CommitIssue.Type.IMPROVEMENT))
-					.when("test").then(() -> types.put(name, CommitIssue.Type.TEST))
-					.when("access").then(() -> types.put(name, CommitIssue.Type.ACCESS))
-					.when("dependency").then(() -> types.put(name, CommitIssue.Type.DEPENDENCY_UPGRADE))
-					.when("request").then(() -> types.put(name, CommitIssue.Type.REQUEST))
-					.when("subtask").then(() -> types.put(name, CommitIssue.Type.SUBTASK))
-					.when("task").then(() -> types.put(name, CommitIssue.Type.TASK))
-					.when("wish").then(() -> types.put(name, CommitIssue.Type.WISH));
+						// Priorities
+						.when("blocker").then(() -> priorities.put(name, CommitIssue.Priority.BLOCKER))
+						.when("breaking", "breaking-java", "pretty blooding important", "critical")
+						.then(() -> priorities.put(name, CommitIssue.Priority.CRITICAL)).when("major", "severity")
+						.then(() -> priorities.put(name, CommitIssue.Priority.MAJOR)).when("minor")
+						.then(() -> priorities.put(name, CommitIssue.Priority.MINOR)).when("trivial")
+						.then(() -> priorities.put(name, CommitIssue.Priority.TRIVIAL))
+						// Types
+						.when("bug").then(() -> types.put(name, CommitIssue.Type.BUG)).when("deprecation")
+						.then(() -> types.put(name, CommitIssue.Type.DEPRECATION)).when("refactor", "change")
+						.then(() -> types.put(name, CommitIssue.Type.REFACTOR)).when("docs", "documentation", "doc")
+						.then(() -> types.put(name, CommitIssue.Type.DOCUMENTATION)).when("feature")
+						.then(() -> types.put(name, CommitIssue.Type.FEATURE))
+						.when("enhancement", "optimization", "improvement")
+						.then(() -> types.put(name, CommitIssue.Type.IMPROVEMENT)).when("test")
+						.then(() -> types.put(name, CommitIssue.Type.TEST)).when("access")
+						.then(() -> types.put(name, CommitIssue.Type.ACCESS)).when("dependency")
+						.then(() -> types.put(name, CommitIssue.Type.DEPENDENCY_UPGRADE)).when("request")
+						.then(() -> types.put(name, CommitIssue.Type.REQUEST)).when("subtask")
+						.then(() -> types.put(name, CommitIssue.Type.SUBTASK)).when("task")
+						.then(() -> types.put(name, CommitIssue.Type.TASK)).when("wish")
+						.then(() -> types.put(name, CommitIssue.Type.WISH));
 
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public Issue getIssue(int issueNumber) {
+		int maxTryCount = 3;
+		int tryCount = 0;
 		IssueService service = new IssueService(client);
-		try {
-			Issue issue = service.getIssue(repository, issueNumber);
-			return issue;
-		} catch (IOException e) {
-			e.printStackTrace();
-			return null;
+		while (true) {
+			try {
+				tryCount++;
+				Issue issue = service.getIssue(repository, issueNumber);
+				return issue;
+			} catch (IOException e) {
+				e.printStackTrace();
+				System.out.println("some sleep time");
+				try {
+					Thread.sleep(120000);//wait for 2 minutes then try again
+				} catch (InterruptedException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}	
+				if (tryCount == maxTryCount)
+					return null;
+			}
 		}
 	}
-	
+
 	public IssueInfoHolder parseIssue(String issueNumber) {
-		if(issueNumber == null) return null;
+		if (issueNumber == null)
+			return null;
 		Issue issue = getIssue(Integer.parseInt(issueNumber));
-		
+
 		IssueInfoHolder holder = new IssueInfoHolder();
 		holder.setName(Integer.toString(issue.getNumber()));
-		
+
 		holder.setLink(issue.getHtmlUrl());
 		holder.setDescription(issue.getTitle());
-		
-		for(Label l : issue.getLabels()) {
-			if(l.getName().startsWith(":")) continue;
-			if(holder.getType() == CommitIssue.Type.NA) {
+
+		for (Label l : issue.getLabels()) {
+			if (l.getName().startsWith(":"))
+				continue;
+			if (holder.getType() == CommitIssue.Type.NA) {
 				CommitIssue.Type type = types.get(l.getName().toLowerCase());
-				if(type != null) {
+				if (type != null) {
 					holder.setType(type);
 				}
 			}
-			if(holder.getPriority() == CommitIssue.Priority.NA) {
+			if (holder.getPriority() == CommitIssue.Priority.NA) {
 				CommitIssue.Priority priority = priorities.get(l.getName().toLowerCase());
-				if(priority != null) {
+				if (priority != null) {
 					holder.setPriority(priority);
 				}
 			}
 		}
-		if(holder.getType() == CommitIssue.Type.NA) holder.setType(CommitIssue.Type.OTHER);
-		
+		if (holder.getType() == CommitIssue.Type.NA)
+			holder.setType(CommitIssue.Type.OTHER);
+
 		return holder;
 	}
-	
-	
-	/*public void loadPullRequests() {
-		pullRequests = new HashMap<>();
-		PullRequestService service = new PullRequestService(client);
-		
-		try {
-			List<PullRequest> result = service.getPullRequests(repository, "closed");
-			for(PullRequest p : result) {
-				System.out.println("PullRequest:"+p.getNumber());
-				pullRequests.put(new Integer(p.getNumber()), p);
-			}
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			pullRequests = null;
-		}
-	}*/
-	
-	/*public PullRequest loadSinglePullRequest(int id) {
-		PullRequestService service = new PullRequestService(client);
-		
-		try {
-			PullRequest result = service.getPullRequest(repository, id);
-			System.out.println("PullRequest:"+result.getNumber());
-			pullRequests.put(new Integer(result.getNumber()), result);
-			return result;
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return null;
-	}*/
-	
-	/*public PullRequest getPullRequest(int id) {
-		if(pullRequests == null) {
-			System.err.println("GitHub pull requests are not loaded!");
-			return null;
-		}
-		return pullRequests.get(new Long(id));
-	}*/
-	
-	/*public Issue getIssue(int pullRequestNumber) {
-		return getIssue(getPullRequest(pullRequestNumber));
-	}
-	
-	public Issue getIssue(PullRequest p) {
-		IssueService service = new IssueService(client);
-		int issueNumber = getIssueNumberFromPullRequest(p);
-		try {
-			Issue issue = service.getIssue(repository, issueNumber);
-			return issue;
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			return null;
-		}
-	}*/
-	
-	
-	/*private int getIssueNumberFromPullRequest(PullRequest p) {
-		String[] tmp = p.getIssueUrl().split("/");
-		return Integer.parseInt(tmp[tmp.length-1]);		
-	}*/
-	
-	/*public IssueInfoHolder parsePullRequestToIssue(String id) {
-		PullRequest p = loadSinglePullRequest(Integer.parseInt(id));
-		Issue issue = getIssue(p);
-		IssueInfoHolder holder = new IssueInfoHolder();
-		holder.setName(Integer.toString(issue.getNumber()));
-		
-		for(Label l : issue.getLabels()) {
-			l.getName();
-		}
-		//parse labels
-		//holder.set
-		return holder;
-	}*/
+
+	/*
+	 * public void loadPullRequests() { pullRequests = new HashMap<>();
+	 * PullRequestService service = new PullRequestService(client);
+	 * 
+	 * try { List<PullRequest> result = service.getPullRequests(repository,
+	 * "closed"); for(PullRequest p : result) {
+	 * System.out.println("PullRequest:"+p.getNumber()); pullRequests.put(new
+	 * Integer(p.getNumber()), p); } } catch (IOException e) { // TODO
+	 * Auto-generated catch block e.printStackTrace(); pullRequests = null; } }
+	 */
+
+	/*
+	 * public PullRequest loadSinglePullRequest(int id) { PullRequestService
+	 * service = new PullRequestService(client);
+	 * 
+	 * try { PullRequest result = service.getPullRequest(repository, id);
+	 * System.out.println("PullRequest:"+result.getNumber());
+	 * pullRequests.put(new Integer(result.getNumber()), result); return result;
+	 * } catch (IOException e) { // TODO Auto-generated catch block
+	 * e.printStackTrace(); } return null; }
+	 */
+
+	/*
+	 * public PullRequest getPullRequest(int id) { if(pullRequests == null) {
+	 * System.err.println("GitHub pull requests are not loaded!"); return null;
+	 * } return pullRequests.get(new Long(id)); }
+	 */
+
+	/*
+	 * public Issue getIssue(int pullRequestNumber) { return
+	 * getIssue(getPullRequest(pullRequestNumber)); }
+	 * 
+	 * public Issue getIssue(PullRequest p) { IssueService service = new
+	 * IssueService(client); int issueNumber = getIssueNumberFromPullRequest(p);
+	 * try { Issue issue = service.getIssue(repository, issueNumber); return
+	 * issue; } catch (IOException e) { // TODO Auto-generated catch block
+	 * e.printStackTrace(); return null; } }
+	 */
+
+	/*
+	 * private int getIssueNumberFromPullRequest(PullRequest p) { String[] tmp =
+	 * p.getIssueUrl().split("/"); return Integer.parseInt(tmp[tmp.length-1]); }
+	 */
+
+	/*
+	 * public IssueInfoHolder parsePullRequestToIssue(String id) { PullRequest p
+	 * = loadSinglePullRequest(Integer.parseInt(id)); Issue issue = getIssue(p);
+	 * IssueInfoHolder holder = new IssueInfoHolder();
+	 * holder.setName(Integer.toString(issue.getNumber()));
+	 * 
+	 * for(Label l : issue.getLabels()) { l.getName(); } //parse labels
+	 * //holder.set return holder; }
+	 */
 }
